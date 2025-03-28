1e1752e0d649f9dc4f3004b899af909411f91480
switon <switon@google.com>
2025-03-28 17:29:18 +0000

    Initial commit - copy of document-search-with-agent-builder - not working

---
+++ document-search-using-agent-engine-agent-framework/frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    apiKey: '<your API Key>',
+    authDomain: '<your Auth Domain>',
+    projectId: '<your Project ID>',
+    storageBucket: "<your storageBucket>",
+    messagingSenderId: "<your messagingSenderId>",
+    appId: "<your appId>",
+    measurementId: "<your measurementId>"
+  },
+  requiredLogin: 'False',
+  backendURL: 'http://localhost:8080/api',
+  chatbotName: 'My New Agent',
+};
